b07976e2acb44df696830ddfe4ccff81cea30251
ArmAtrix123 <alexstor2003@gmail.com>
2022-10-25 02:26:08 +0300

    aboba

--- flutter_first_project/lib/main.dart
+++ flutter_first_project/lib/main.dart
@@ -7,40 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Макетики'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +28,274 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // return Scaffold(
+    //   backgroundColor: const Color.fromARGB(255, 3, 157, 162),
+    //   body: Center(
+    //     child: Column(
+    //       children: [
+    //         Padding(
+    //           padding: EdgeInsets.fromLTRB(0, 118, 0, 0),
+    //           child: Container(
+    //             alignment: Alignment.center,
+    //             child: Text(
+    //               'medinow',
+    //               textAlign: TextAlign.center,
+    //               style: TextStyle(
+    //                 fontSize: 40,
+    //                 fontWeight: FontWeight.bold,
+    //                 color: Colors.white,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         Padding(
+    //           padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
+    //           child: Container(
+    //             alignment: Alignment.center,
+    //             child: Text(
+    //               'Meditate with us',
+    //               textAlign: TextAlign.center,
+    //               style: TextStyle(
+    //                 fontSize: 17,
+    //                 fontWeight: FontWeight.normal,
+    //                 color: Colors.white,
+    //               ),
+    //             ),
+    //           ),
+    //         ),
+    //         Padding(
+    //           padding: EdgeInsets.fromLTRB(24, 55, 24, 0),
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.stretch,
+    //             children: [
+    //               ElevatedButton(
+    //                   onPressed: () => {},
+    //                   style: ElevatedButton.styleFrom(
+    //                       primary: Colors.white,
+    //                       padding: const EdgeInsets.all(20),
+    //                       shape: RoundedRectangleBorder(
+    //                           borderRadius: BorderRadius.circular(50))),
+    //                   child: Text(
+    //                     'Sigh in with Apple',
+    //                     textDirection: TextDirection.ltr,
+    //                     style: TextStyle(
+    //                       fontSize: 16,
+    //                       fontWeight: FontWeight.bold,
+    //                       fontStyle: FontStyle.normal,
+    //                       color: Colors.black,
+    //                     ),
+    //                   ))
+    //             ],
+    //           ),
+    //         ),
+    //         Padding(
+    //           padding: EdgeInsets.fromLTRB(24, 12, 24, 0),
+    //           child: Column(
+    //               crossAxisAlignment: CrossAxisAlignment.stretch,
+    //               children: [
+    //                 ElevatedButton(
+    //                     onPressed: () => {},
+    //                     style: ElevatedButton.styleFrom(
+    //                         primary: Color.fromRGBO(205, 253, 254, 1),
+    //                         padding: const EdgeInsets.all(20),
+    //                         shape: RoundedRectangleBorder(
+    //                             borderRadius: BorderRadius.circular(50))),
+    //                     child: Text(
+    //                       'Continue with Email or Phone',
+    //                       textDirection: TextDirection.ltr,
+    //                       style: TextStyle(
+    //                         fontSize: 16,
+    //                         fontWeight: FontWeight.bold,
+    //                         fontStyle: FontStyle.normal,
+    //                         color: Colors.black,
+    //                       ),
+    //                     ))
+    //               ]),
+    //         ),
+    //         Padding(
+    //           padding: EdgeInsets.fromLTRB(0, 12, 0, 0),
+    //           child: Column(
+    //               crossAxisAlignment: CrossAxisAlignment.center,
+    //               children: [
+    //                 InkWell(
+    //                     onTap: () {},
+    //                     child: Text(
+    //                       'Continue With Google',
+    //                       textDirection: TextDirection.ltr,
+    //                       style: TextStyle(
+    //                         fontSize: 16,
+    //                         fontWeight: FontWeight.bold,
+    //                         fontStyle: FontStyle.normal,
+    //                         color: Colors.white,
+    //                       ),
+    //                     ))
+    //               ]),
+    //         ),
+    //         Padding(
+    //             padding: EdgeInsets.fromLTRB(0, 110, 0, 0),
+    //             child: Image.asset('assets/chelick.png'))
+    //       ],
+    //     ),
+    //   ),
+    // );
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        body: Center(
+            child: Column(children: [
+      Stack(children: [
+        Container(
+          child: Image.asset('assets/Image.png'),
+          width: 341,
+          height: 241,
+          margin: EdgeInsets.fromLTRB(24, 36, 24, 0),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Container(
+          child: Image.asset('assets/Frame23.png'),
+          width: 343,
+          height: 219,
+          margin: EdgeInsets.fromLTRB(24, 53, 24, 0),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(30, 300, 0, 0),
+          child: Text(
+            'Peter Mach',
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 12),
+            // fontFamily: 'PlusJakartaSans'),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(33, 320, 0, 0),
+          child: Text(
+            'Mind Deep Relax',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            // fontFamily: 'PlusJakartaSans'),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(33, 350, 28, 0),
+          child: Text(
+            'Join the Community as we prepare over 33'
+            'days to relax and feel joy with the mind'
+            'and happnies session across the World.',
+            style: TextStyle(fontWeight: FontWeight.normal, fontSize: 15),
+            // fontFamily: 'PlusJakartaSans'),
+          ),
+        ),
+        Container(
+            width: 342,
+            margin: EdgeInsets.fromLTRB(24, 430, 0, 0),
+            child: ElevatedButton(
+                onPressed: () => {},
+                style: ElevatedButton.styleFrom(
+                    primary: Color.fromRGBO(3, 158, 162, 1),
+                    padding: const EdgeInsets.all(20),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50))),
+                child: const Text(
+                  ' Play Next Session',
+                  textDirection: TextDirection.ltr,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.white,
+                  ),
+                ))),
+        Container(
+          child: Image.asset(
+            'Assets/Group2.png',
+          ),
+          margin: EdgeInsets.fromLTRB(108, 451, 0, 0),
+        ),
+//Первая кнопка
+        Container(
+          child: Image.asset('assets/shape.png'),
+          margin: EdgeInsets.fromLTRB(23, 500, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'Sweet Memories',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 500, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'December 29 Pre-Launch',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 12,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 520, 0, 0),
+        ),
+        Container(
+          child: Image.asset('assets/Group3.png'),
+          margin: EdgeInsets.fromLTRB(327, 518, 0, 0),
+        ),
+//2 кнопка
+        Container(
+          child: Image.asset('assets/Group4.png'),
+          margin: EdgeInsets.fromLTRB(23, 550, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'A Day Dream',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 550, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'December 29 Pre-Launch',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 12,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 575, 0, 0),
+        ),
+        Container(
+          child: Image.asset('assets/Group3.png'),
+          margin: EdgeInsets.fromLTRB(327, 560, 0, 0),
+        ),
+//3 кнопка
+        Container(
+          child: Image.asset('assets/Group5.png'),
+          margin: EdgeInsets.fromLTRB(23, 600, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'Mind Explore',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 600, 0, 0),
+        ),
+        Container(
+          child: Text(
+            'December 29 Pre-Launch',
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 12,
+            ),
+          ),
+          margin: EdgeInsets.fromLTRB(78, 625, 0, 0),
+        ),
+        Container(
+          child: Image.asset('assets/Group3.png'),
+          margin: EdgeInsets.fromLTRB(327, 610, 0, 0),
+        ),
+      ])
+    ])));
   }
 }
